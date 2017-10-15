7572733ec5bc4ed6f00d5ace963b93d4be95e7eb
Maria Tereza Cabral <mtereza.cabral@gmail.com>
2017-10-14 23:37:53 -0400

    Use raw input instead

--- create_relationship.py
+++ create_relationship.py
@@ -1,9 +1,9 @@
 character = { "Tyrion" : 1, "Jaime" : 2, "Cersei" : 3, "Daenerys" : 4, "Eddard" : 5,
                       "Catelyn": 6, "Petyr" : 7, "Jon" : 8, "Davos" : 9, "Robert" : 10,
                                     "Joffrey" : 11, "Margaery" : 12, "Loras" : 13, "Renly" : 14, "Stannis" : 15, "Robb" : 16, "Talisa" : 17 }
-print character["Tyrion"]
-c1 = input("character name: ")
-c2 = input("character name 2: ")
+
+c1 = raw_input("character name: ")
+c2 = raw_input("character name 2: ")
 id1 = character[c1]
 id2 = character[c2]
 type = raw_input("type of relationship: ")
